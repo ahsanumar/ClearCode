809f977e2ab4df4099fd6e5d75336e7cf49bcf39
ahsanumar <32022750+ahsanumar@users.noreply.github.com>
2018-04-10 21:43:34 +0500

    Please enter the commit message for your changes. Lines starting

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express(),
+    pug = require("pug"),
+    port = 6,
+    successMesssage = "Server started at http://localhost:"+port
+
+app.set("view engine", "pug")
+app.set("views", __dirname+"/views")
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+app.listen(port, console.log(successMesssage))
