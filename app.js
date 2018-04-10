5351f6e4188901c962a51b9045d4b1b165e37489
ahsanumar <32022750+ahsanumar@users.noreply.github.com>
2018-04-11 00:39:12 +0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app = express(),
+    pug = require("pug"),
+    port = 6,
+    successMesssage = "Server started at http://localhost:"+port
+
+app.set("view engine", "pug")
+app.set("views", __dirname+"/views")
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+app.get("/home", function(req, res){
+    res.render("index")
+})
+app.get("/about", function(req, res){
+    res.send('<h1>About Us</h1>')
+})
+app.listen(port, console.log(successMesssage))
